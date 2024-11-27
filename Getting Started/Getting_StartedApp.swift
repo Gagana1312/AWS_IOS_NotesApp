30d54aa053989b94b886c529ac3cd633d06a7104
Gagana1312 <ananda.g@northeastern.edu>
2024-11-27 11:55:11 -0700

    Initial Commit

---
+++ Getting Started/Getting_StartedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Getting_StartedApp.swift
+//  Getting Started
+//
+//  Created by Gagana Ananda on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Getting_StartedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
